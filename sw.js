5d7d6b1b107fdaefadef7a021f123ec24f5473c2
samir-kamble <samir.kamble@gmail.com>
2016-09-21 18:20:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
 //Service Worker File
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/main.js'
+];
+
 self.addEventListener('install', function(event) {
   // Perform install steps
-  console.log('install event started');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache for saving files');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
