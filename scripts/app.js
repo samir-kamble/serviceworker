fa297013a4d338de9f76049f1583f85f846fd7d3
samir-kamble <samir.kamble@gmail.com>
2016-09-21 20:05:12 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -149,7 +149,34 @@
     });
   };
 
-  var fakeForecast = {
+var initialWeatherForecast = {  
+  key: 'newyork',  
+  label: 'New York, NY',  
+  currently: {  
+    time: 1453489481,  
+    summary: 'Clear',  
+    icon: 'partly-cloudy-day',  
+    temperature: 52.74,  
+    apparentTemperature: 74.34,  
+    precipProbability: 0.20,  
+    humidity: 0.77,  
+    windBearing: 125,  
+    windSpeed: 1.52  
+  },  
+  daily: {  
+    data: [  
+      {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'rain', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'snow', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'fog', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'wind', temperatureMax: 55, temperatureMin: 34},  
+      {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}  
+    ]  
+  }  
+};
+
+  /*var fakeForecast = {
     key: 'newyork',
     label: 'New York, NY',
     currently: {
@@ -176,7 +203,7 @@
     }
   };
   // Uncomment the line below to test with the provided fake data
-   app.updateForecastCard(fakeForecast);
+   //app.updateForecastCard(fakeForecast);*/
 
 })();
 
