589923427ef2ec69954be68eeba6824f96e51174
samir-kamble <samir.kamble@gmail.com>
2016-09-21 18:43:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -58,3 +58,10 @@ self.addEventListener('fetch', function(event) {
       })
     );
 });
+
+//Fetching content
+cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+  return new Request(urlToPrefetch, { mode: 'no-cors' });
+})).then(function() {
+  console.log('All resources have been fetched and cached.');
+});
