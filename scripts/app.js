e702e6d80a9016f135204159aa4fae4755d23512
samir-kamble <samir.kamble@gmail.com>
2016-09-22 12:03:20 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,35 @@
 (function() {
   'use strict';
 
+  // Insert injected weather forecast here
+  var initialWeatherForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
+  // Don't forget to add hasRequestPending: false
   var app = {
     isLoading: true,
     visibleCards: {},
@@ -38,6 +67,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -124,7 +154,8 @@
   app.getForecast = function(key, label) {
     var url = 'https://publicdata-weather.firebaseio.com/';
     url += key + '.json';
-    // Make the XHR to get the data, then update the card
+    // Make request to the cache here
+    // Remember to set app.hasRequestPending = true!
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
@@ -132,7 +163,8 @@
           var response = JSON.parse(request.response);
           response.key = key;
           response.label = label;
-          app.hasRequestPending = false;
+          // Remember to set app.hasRequestPending = false!
+          console.log('[App] Forecast Updated From Network');
           app.updateForecastCard(response);
         }
       }
@@ -149,97 +181,44 @@
     });
   };
 
-var initialWeatherForecast = {  
-  key: 'newyork',  
-  label: 'New York, NY',  
-  currently: {  
-    time: 1453489481,  
-    summary: 'Clear',  
-    icon: 'partly-cloudy-day',  
-    temperature: 52.74,  
-    apparentTemperature: 74.34,  
-    precipProbability: 0.20,  
-    humidity: 0.77,  
-    windBearing: 125,  
-    windSpeed: 1.52  
-  },  
-  daily: {  
-    data: [  
-      {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'rain', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'snow', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'fog', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'wind', temperatureMax: 55, temperatureMin: 34},  
-      {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}  
-    ]  
-  }  
-};
+  // Save list of cities to localStorage, see note below about localStorage.
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    // IMPORTANT: See notes about use of localStorage.
+    localStorage.selectedCities = selectedCities;
+  };
 
-  /*var fakeForecast = {
-    key: 'newyork',
-    label: 'New York, NY',
-    currently: {
-      time: 1453489481,
-      summary: 'Clear',
-      icon: 'partly-cloudy-day',
-      temperature: 30,
-      apparentTemperature: 21,
-      precipProbability: 0.80,
-      humidity: 0.17,
-      windBearing: 125,
-      windSpeed: 1.52
-    },
-    daily: {
-      data: [
-        {icon: 'clear-day', temperatureMax: 36, temperatureMin: 31},
-        {icon: 'rain', temperatureMax: 34, temperatureMin: 28},
-        {icon: 'snow', temperatureMax: 31, temperatureMin: 17},
-        {icon: 'sleet', temperatureMax: 38, temperatureMin: 31},
-        {icon: 'fog', temperatureMax: 40, temperatureMin: 36},
-        {icon: 'wind', temperatureMax: 35, temperatureMin: 29},
-        {icon: 'partly-cloudy-day', temperatureMax: 42, temperatureMin: 40}
-      ]
-    }
-  };*/
-  // Uncomment the line below to test with the provided fake data
-   app.updateForecastCard(initialWeatherForecast);
+  /*****************************************************************************
+   *
+   * Code required to start the app
+   *
+   * NOTE: To simplify this getting started guide, we've used localStorage.
+   *   localStorage is a syncronous API and has serious performance
+   *   implications. It should not be used in production applications!
+   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+   *
+   ****************************************************************************/
 
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
 
-// Save list of cities to localStorage, see note below about localStorage.
-app.saveSelectedCities = function() {
-  var selectedCities = JSON.stringify(app.selectedCities);
-  // IMPORTANT: See notes about use of localStorage.
-  localStorage.selectedCities = selectedCities;
-};
-
-
-/****************************************************************************   
- *
- * Code required to start the app
- *
- * NOTE: To simplify this getting started guide, we've used localStorage.
- *   localStorage is a synchronous API and has serious performance
- *   implications. It should not be used in production applications!
- *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
- *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
- *
- ****************************************************************************/
-
-app.selectedCities = localStorage.selectedCities;
-if (app.selectedCities) {
-  app.selectedCities = JSON.parse(app.selectedCities);
-  app.selectedCities.forEach(function(city) {
-    app.getForecast(city.key, city.label);
-  });
-} else {
-  app.updateForecastCard(initialWeatherForecast);
-  app.selectedCities = [
-    {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
-  ];
-  app.saveSelectedCities();
-}
-
+  // Add feature check for Service Workers here
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 
 })();
-
