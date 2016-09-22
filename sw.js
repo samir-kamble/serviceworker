0072c91126bc2af72b86a27c7cafb1e64b547645
samir-kamble <samir.kamble@gmail.com>
2016-09-22 12:44:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ var filesToCache = [];
 self.addEventListener('install', function(e) {  
   console.log('[ServiceWorker] Install');  
   e.waitUntil(  
-    caches.open(cacheName).then(function(cache) {  
+    cache.open(cacheName).then(function(cache) {  
       console.log('[ServiceWorker] Caching app shell');  
       return cache.addAll(filesToCache);  
     })  
