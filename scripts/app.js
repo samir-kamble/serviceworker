f2352f3c399385c190b0cae4cfd66cb33f0eca57
samir-kamble <samir.kamble@gmail.com>
2016-09-22 12:20:08 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
 
 (function() {
+  
+  if('serviceWorker' in navigator) {  
+  navigator.serviceWorker  
+           .register('/service-worker.js')  
+           .then(function() { console.log('Service Worker Registered'); });  
+  }
+  
+  
   'use strict';
 
   // Insert injected weather forecast here
